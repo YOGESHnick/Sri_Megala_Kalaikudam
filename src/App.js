6ba32c639f706d42f53bc4538f97b731da4d819e
Yogesh Shamlin Shinanth <yogeshshamlin@gmail.com>
2024-06-23 21:40:29 +0530

    2 sections done

--- src/App.js
+++ src/App.js
@@ -1,98 +1,231 @@
-import React from 'react';
+// import React from 'react';
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
+// import { SnackbarProvider } from 'notistack';
+// import { Navbar } from './components/NavBar';
+// // import Footer from './components/Footer';
+// import Home from './pages/Home';
+// import Contact from './pages/Contact';
+
+// const theme = createTheme({
+//   typography: {
+//     fontFamily: 'Noto Sans, sans-serif',
+//     serif: {
+//       fontFamily: 'Noto Serif, serif',
+//     },
+//     bebasNeue: {
+//       fontFamily: 'Bebas Neue, sans-serif',
+//     },
+//     junge: {
+//       fontFamily: 'Junge',
+//     },
+//     ptSerif: {
+//       fontFamily: 'PT Serif, serif',
+//     },
+//     subtitle1: {
+//       fontSize: 24,
+//       color: '#101010',
+//       fontFamily: 'Noto Serif, serif',
+//     },
+//     body1: {
+//       '@media (max-width: 960px)': {
+//         fontSize: 16,
+//       },
+//       color: '#000000',
+//       fontWeight: 400,
+//       fontSize: 20,
+//     },
+//     body2: {
+//       color: '#101010',
+//       fontSize: 20,
+//       fontWeight: 400,
+//       lineHeight: 1.4,
+//     },
+//     h1: {
+//       '@media (max-width: 960px)': {
+//         fontSize: 40,
+//       },
+//       fontSize: 60,
+//       fontWeight: 700,
+//       fontFamily: 'Noto Serif, serif',
+//     },
+//     h2: {
+//       '@media (max-width: 960px)': {
+//         fontSize: 32,
+//       },
+//       color: '#1F1F1F',
+//       fontSize: 48,
+//       fontWeight: 700,
+//       fontFamily: 'Noto Serif, serif',
+//     },
+//     h3: {
+//       '@media (max-width: 960px)': {
+//         fontSize: 28,
+//       },
+//       color: '#1F1F1F',
+//       fontSize: 40,
+//       fontWeight: 700,
+//       fontFamily: 'Noto Serif, serif',
+//     },
+//     h4: {
+//       '@media (max-width: 960px)': {
+//         fontSize: 24,
+//       },
+//       fontSize: 32,
+//       fontWeight: 700,
+//       fontFamily: 'Noto Serif, serif',
+//     },
+//     h5: {
+//       fontSize: 20,
+//       fontWeight: 700,
+//       fontFamily: 'Noto Serif, serif',
+//     },
+//     h6: {
+//       fontFamily: 'Noto Serif, serif',
+//       fontWeight: 700,
+//     }
+//   },
+//   palette: {
+//     brand: "#B19540",
+//     primary: {
+//       main: "#B19540",
+//     }
+//   },
+//   shape: {
+//     borderRadius: 0,
+//   },
+// });
+
+// export default function App() {
+//   return (
+//     <ThemeProvider theme={theme}>
+//       <CssBaseline />
+//       <SnackbarProvider>
+//         <div className="App">
+//           <Router>
+//             <Navbar />
+//             <Routes>
+//               <Route path="/" element={<Home />} />
+//               <Route path="/contact" element={<Contact />} />
+//             </Routes>
+//             {/* <Footer /> */}
+//           </Router>
+//         </div>
+//       </SnackbarProvider>
+//     </ThemeProvider>
+//   );
+// }
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
-import { SnackbarProvider } from 'notistack';
-import { Navbar } from './components/NavBar';
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+import { SnackbarProvider } from "notistack";
+import { Navbar } from "./components/NavBar";
 // import Footer from './components/Footer';
-import Home from './pages/Home';
-import Contact from './pages/Contact';
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
 
 const theme = createTheme({
   typography: {
-    fontFamily: 'Noto Sans, sans-serif',
+    fontFamily: "Noto Sans, sans-serif",
     serif: {
-      fontFamily: 'Noto Serif, serif',
+      fontFamily: "Noto Serif, serif",
     },
     bebasNeue: {
-      fontFamily: 'Bebas Neue, sans-serif',
+      fontFamily: "Bebas Neue, sans-serif",
     },
     junge: {
-      fontFamily: 'Junge',
+      fontFamily: "Junge",
     },
     ptSerif: {
-      fontFamily: 'PT Serif, serif',
+      fontFamily: "PT Serif, serif",
     },
     subtitle1: {
       fontSize: 24,
-      color: '#101010',
-      fontFamily: 'Noto Serif, serif',
+      color: "#101010",
+      fontFamily: "Noto Serif, serif",
     },
     body1: {
-      '@media (max-width: 960px)': { 
+      "@media (max-width: 960px)": {
         fontSize: 16,
       },
-      color: '#000000',
+      color: "#000000",
       fontWeight: 400,
       fontSize: 20,
     },
     body2: {
-      color: '#101010',
+      color: "#101010",
       fontSize: 20,
       fontWeight: 400,
       lineHeight: 1.4,
     },
     h1: {
-      '@media (max-width: 960px)': { 
+      "@media (max-width: 960px)": {
         fontSize: 40,
       },
       fontSize: 60,
       fontWeight: 700,
-      fontFamily: 'Noto Serif, serif',
+      fontFamily: "Noto Serif, serif",
     },
     h2: {
-      '@media (max-width: 960px)': { 
+      "@media (max-width: 960px)": {
         fontSize: 32,
       },
-      color: '#1F1F1F',
+      color: "#1F1F1F",
       fontSize: 48,
       fontWeight: 700,
-      fontFamily: 'Noto Serif, serif',
+      fontFamily: "Noto Serif, serif",
     },
     h3: {
-      '@media (max-width: 960px)': {
+      "@media (max-width: 960px)": {
         fontSize: 28,
       },
-      color: '#1F1F1F',
+      color: "#1F1F1F",
       fontSize: 40,
       fontWeight: 700,
-      fontFamily: 'Noto Serif, serif',
+      fontFamily: "Noto Serif, serif",
     },
     h4: {
-      '@media (max-width: 960px)': {
+      "@media (max-width: 960px)": {
         fontSize: 24,
       },
       fontSize: 32,
       fontWeight: 700,
-      fontFamily: 'Noto Serif, serif',
+      fontFamily: "Noto Serif, serif",
     },
     h5: {
       fontSize: 20,
       fontWeight: 700,
-      fontFamily: 'Noto Serif, serif',
+      fontFamily: "Noto Serif, serif",
     },
     h6: {
-      fontFamily: 'Noto Serif, serif',
+      fontFamily: "Noto Serif, serif",
       fontWeight: 700,
-    }
+    },
   },
   palette: {
     brand: "#B19540",
     primary: {
       main: "#B19540",
-    }
+    },
   },
   shape: {
     borderRadius: 0,
+  },
+  components: {
+    MuiTypography: {
+      styleOverrides: {
+        root: {
+          backgroundColor: "transparent",
+        },
+      },
+    },
+    MuiGrid: {
+      styleOverrides: {
+        root: {
+          backgroundColor: "transparent",
+        },
+      },
+    },
   },
 });
 
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,41 +1,147 @@
-import React from 'react';
+import React from "react";
 import {
   Box,
-  Button,
   Typography,
-  Stack,
-  Container,
-  Grid,
   useTheme,
   useMediaQuery,
+  styled,
+  Grid,
+  Button,
 } from "@mui/material";
+import { Link } from "react-router-dom";
+import Introo from "../assets/introo.png";
+import Hero from "../assets/Hero.png";
+import Hero2 from "../assets/Hero2.png";
 
+const Image = styled("img")({
+  maxWidth: "70%",
+  height: "auto",
+  display: "block",
+  margin: "auto",
+  backgroundColor: "transparent",
+});
 
 const Home = () => {
+  const theme = useTheme();
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
   return (
     <div>
-      <Box>
-      <Typography
-          variant="junge"
-          display="block"
-          color="#593C35"
-          mb={2}
-          fontSize={{ md: 48, xs: 30 }}
-        >
-          SRI MEGALA KALAIKUDAM
-        </Typography>
+      <Box textAlign="center" mx="auto">
+        <Image
+          src={Introo}
+          alt="Natarajar image"
+          style={{ width: isSmallScreen ? "100%" : "auto" }}
+        />
+      </Box>
+
+      {/* D I S C O V E R   E S S E N C E  O F  B H A R A T H A N A T Y A M */}
+
+      <Box bgcolor="#B12C2C" textAlign="center" p={10}>
+        <Grid container spacing={2} alignItems="center" justifyContent="center">
+          <Grid item xs={12} md={6} sx={{ m: 0 }} textAlign="left">
+            <Typography
+              variant="bebasNeue"
+              fontSize={{ md: 48, xs: 30 }}
+              color="#F7C97E"
+              textAlign="left"
+              display="block"
+            >
+              DISCOVER ESSENCE OF BHARATHANATYAM
+            </Typography>
+            <Typography
+              variant="ptSerif"
+              textAlign="left"
+              fontSize={{ md: 20, xs: 18 }}
+              color="white"
+              sx={{ marginBottom: 2 }}
+              display="block"
+            >
+              Immerse yourself in the ancient art of Bharatanatyam with our
+              all-encompassing learning program. We present this age-old dance
+              form, cultivating your artistic talents and enhancing your
+              cultural appreciation. Embark on a path where tradition merges
+              with excellence, and each gesture narrates a tale of elegance,
+              rhythm, and dedication.
+            </Typography>
+            <button>
+              <Link className="Link" to="/contact">
+                Why us?
+              </Link>
+            </button>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <Image
+              src={Hero}
+              alt="Natarajar image"
+              style={{
+                width: isSmallScreen ? "60%" : "50%",
+                maxWidth: "200px",
+                backgroundColor: "transparent",
+              }}
+            />
+          </Grid>
+        </Grid>
+      </Box>
+
+      {/* A B O U T   T H E   G U R U  */}
+
+      <Box textAlign="center" p={10}>
         <Typography
           variant="bebasNeue"
-          display="block"
-          color="#593C35"
-          mb={2}
           fontSize={{ md: 48, xs: 30 }}
+          color="black"
+          display="block"
+          mb={5}
         >
-          ADMIN BLOCK, LOYOLA COLLEGE, CHENNAI
+          ABOUT THE GURU
         </Typography>
+        <Grid container spacing={2} alignItems="center" justifyContent="center">
+          <Grid item xs={12} md={6}>
+            <Image
+              src={Hero2}
+              alt="Natarajar image"
+              style={{
+                width: isSmallScreen ? "80%" : "90%",
+                maxWidth: "300px",
+                backgroundColor: "transparent",
+              }}
+            />
+          </Grid>
+          <Grid item xs={12} md={6} sx={{ m: 0 }} textAlign="left">
+            <Typography
+              variant="bebasNeue"
+              fontSize={{ md: 48, xs: 30 }}
+              color="black"
+              textAlign="left"
+              display="block"
+            >
+              Deepika l
+            </Typography>
+            <Typography
+              variant="ptSerif"
+              textAlign="left"
+              fontSize={{ md: 20, xs: 18 }}
+              color="black"
+              sx={{ marginBottom: 2 }}
+              display="block"
+            >
+              A highly accomplished Bharatanatyam dancer and dedicated teacher
+              with a B.A. and M.A. in Bharatanatyam from Tamil University and
+              Tamil Nadu Dr. J. Jayalalithaa Music and Fine Arts University,
+              respectively. She has extensive professional experience and
+              numerous accolades, committed to nurturing her students' artistic
+              talents and fostering their growth in dance.
+            </Typography>
+            <button>
+              <Link className="Link" to="/contact">
+                Know More
+              </Link>
+            </button>
+          </Grid>
+        </Grid>
       </Box>
     </div>
   );
-}
+};
 
 export default Home;
